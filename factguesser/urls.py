1f28b211fd6ed490945f6faa1e9c446776ba9581
Ronja Kaariainen <ronja.kaariainen@gmail.com>
2017-10-17 12:21:55 +0000

    Merged the settings and files that were generated by c9 with those generated by the tutorial ('startproject' command)

--- factguesser/urls.py
+++ factguesser/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from tutorial.quickstart import views
+from factguesser import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
--- factguesser/views.py
+++ factguesser/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
