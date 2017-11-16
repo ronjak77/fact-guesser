dbaff59e6866b3bb26dde0f8ce982f0c8151afc1
Ronja Kaariainen <ronja.kaariainen@gmail.com>
2017-11-16 15:14:46 +0000

    Adding schema and schema view

--- factguesser/urls.py
+++ factguesser/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
-from factguesser import views
-from rest_framework.routers import DefaultRouter
-from factguesser.views import PropositionViewSet, PropositionCountView, UserViewSet, AnswerViewSet
 from rest_framework import renderers
+from rest_framework.schemas import get_schema_view
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
+from factguesser import views
+from factguesser.views import PropositionViewSet, PropositionCountView, UserViewSet, AnswerViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -11,10 +12,13 @@ router.register(r'propositions', views.PropositionViewSet)
 router.register(r'users', views.UserViewSet)
 router.register(r'answers', views.AnswerViewSet)
 
+schema_view = get_schema_view(title='Fact listing API')
+
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^schema/$', schema_view),
     url(r'^proposition-count', views.PropositionCountView, name='propositioncount'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
