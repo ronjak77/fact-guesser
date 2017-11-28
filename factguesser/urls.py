feea4380447b34270ea9bd9115fef1ef85983b29
Ronja Kaariainen <ronja.kaariainen@gmail.com>
2017-11-28 16:18:30 +0000

    Allow non-authenticated users to access API root

--- factguesser/urls.py
+++ factguesser/urls.py
@@ -5,9 +5,25 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 from factguesser import views
 from factguesser.views import PropositionViewSet, UserViewSet, AnswerViewSet
+from rest_framework import routers
+from rest_framework import permissions
 
-# Create a router and register our viewsets with it.
-router = DefaultRouter()
+# Create a custom router and register our viewsets with it.
+# The router extends the Default router and uses a custom APIRootView.
+# This way we can keep DEFAULT_PERMISSION_CLASSES strict and still allow
+# access to the API root for non-authenticated users.
+
+class PropositionAppView(routers.APIRootView):
+    """
+    An API for creating Propositions and Answers related to them.
+    """
+    permission_classes = (permissions.AllowAny,)
+
+class CustomRouter(routers.DefaultRouter):
+    APIRootView = PropositionAppView
+
+router = CustomRouter()
+
 router.register(r'propositions', views.PropositionViewSet)
 router.register(r'users', views.UserViewSet)
 router.register(r'answers', views.AnswerViewSet)
